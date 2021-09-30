425b750a22faf63e5b1d9c4762c0d233b2f380c8
Fabian Brash <fabianbrash@gmail.com>
2021-09-30 12:14:49 -0400

    added logic to read json file from s3 bucket or any url

--- api/main.py
+++ api/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 import json
+import urllib.request
 
 app = FastAPI()
+s3 = True
 
-with open("USD.json") as file:
-    d = json.load(file)
+if s3:
+    with urllib.request.urlopen("https://s3.amazonaws.com/fb_bin/USD.json") as f:
+        s3_data = json.load(f)
+else:
+    with open("USD.json") as file:
+        d = json.load(file)
 
 @app.get("/", tags=["root"])
 def read_root() -> dict:
-    return {"data": d}
+    if s3:
+        return {"data": s3_data}
+    else:
+        return {"data": d}
